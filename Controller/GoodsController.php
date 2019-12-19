3959733fb5d88a59e742e45d27138f6ff997fb27
kale <276925701@qq.com>
2019-12-19 13:57:34 +0800

    测试

---
+++ Controller/GoodsController.php
@@ -0,0 +1,14 @@
+<?php
+class GoodsController{
+    public $username;
+    public $password;
+    public function __construct($username,$password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function login(){
+
+    }
+}
